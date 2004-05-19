76ecbc188fde2110ec9eea8f0c4ef01088474cd0
malcolm <unknown>
2004-05-19 11:24:59 +0000

    [cpphs @ 2004-05-19 11:24:59 by malcolm]
    Allow #include to expand a symbol definition to find file.  (Doesn't expand
    macro definitions properly.)

--- ReadFirst.hs
+++ ReadFirst.hs
@@ -19,24 +19,23 @@ import IO        (readFile)
 import Directory (doesFileExist)
 import List      (intersperse)
 import Position  (Posn)
+import SymTab    (SymTab,lookupST)
 
 -- | Attempt to read the given file from any location within the search path.
 --   The first location found is returned, together with the file content.
 --   (The current directory is always searched first.)
-readFirst :: String	-- ^ filename
-	-> Posn		-- ^ inclusion point
-	-> [String]	-- ^ search path
+readFirst :: String		-- ^ filename
+	-> Posn			-- ^ inclusion point
+	-> [String]		-- ^ search path
+	-> SymTab String	-- ^ #defined symbols
 	-> IO ( FilePath	-- ^ discovered file name, inc path
               , String		-- ^ file contents
               )
 
-readFirst name demand path =
+readFirst name demand path syms =
     try (".":path)	-- always search current directory first
   where
-    realname = case name of
-                 ('"':ns) -> init ns
-                 ('<':ns) -> init ns
-                 _        -> name
+    realname = real name syms
     try [] = error ("Can't find file \""++realname++"\" in directories\n\t"
                     ++concat (intersperse "\n\t" (".":path))
                     ++"\n  Asked for by: "++show demand)
@@ -46,4 +45,11 @@ readFirst name demand path =
         if not ok then try ps
           else do content <- readFile file
                   return (file,content)
+
+real name syms = case name of
+                   ('"':ns) -> init ns
+                   ('<':ns) -> init ns
+                   _        -> case lookupST name syms of
+                                 Nothing -> name
+                                 Just f  -> real f syms
 
