dbe5cef62e24018eb365f59740cd4afae2cb4fb9
malcolm <unknown>
2005-04-01 13:33:03 +0000

    [cpphs @ 2005-04-01 13:33:03 by malcolm]
    Change licence text.

--- cpphs.hs
+++ cpphs.hs
@@ -1,9 +1,15 @@
 {-
--- The main program for cpphs, a simple C pre-processor written in Haskell.
+-- The main program wrapper for cpphs, a simple C pre-processor
+-- written in Haskell.
 
--- Copyright (c) 2004 Malcolm Wallace
--- This file is GPL, although the libraries it uses are either standard
--- Haskell'98 or distributed under the LGPL.
+-- Author: Malcolm Wallace, 2004
+-- This file is placed into the public domain, as it is so trivial that it
+-- probably is unprotectable.  Note however, that it uses modules 'RunCpphs',
+-- which is GPL, and 'System' which is Haskell'98.  All other modules used
+-- in turn by those are either distributed under the LGPL, or are Haskell'98.
+--
+-- Thus, when compiled as a standalone executable, this program will fall
+-- under the GPL.
 -}
 module Main where
 
