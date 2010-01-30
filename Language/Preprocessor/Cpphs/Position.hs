bde9934c81481cd6c5f61f01aa93f462f280386e
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2010-01-30 12:53:51 +0000

    Improve the haddockumentation of Language.Preprocessor.Cpphs.Position.

--- Language/Preprocessor/Cpphs/Position.hs
+++ Language/Preprocessor/Cpphs/Position.hs
@@ -36,31 +36,38 @@ instance Show Posn where
                                     Just p  -> showString "\n    used by  " .
                                                shows p )
 
--- | Constructor
+-- | Constructor.  Argument is filename.
 newfile :: String -> Posn
 newfile name = Pn name 1 1 Nothing
 
--- | Updates
+-- | Increment column number by given quantity.
 addcol :: Int -> Posn -> Posn
 addcol n (Pn f r c i) = Pn f r (c+n) i
 
-newline, tab :: Posn -> Posn
+-- | Increment row number, reset column to 1.
+newline :: Posn -> Posn
 --newline (Pn f r _ i) = Pn f (r+1) 1 i
 newline (Pn f r _ i) = let r' = r+1 in r' `seq` Pn f r' 1 i
+
+-- | Increment column number, tab stops are every 8 chars.
+tab     :: Posn -> Posn
 tab     (Pn f r c i) = Pn f r (((c`div`8)+1)*8) i
 
+-- | Increment row number by given quantity.
 newlines :: Int -> Posn -> Posn
 newlines n (Pn f r _ i) = Pn f (r+n) 1 i
 
+-- | Update position with a new row, and possible filename.
 newpos :: Int -> Maybe String -> Posn -> Posn
 newpos r Nothing  (Pn f _ c i) = Pn f r c i
 newpos r (Just ('"':f)) (Pn _ _ c i) = Pn (init f) r c i
 newpos r (Just f)       (Pn _ _ c i) = Pn f r c i
 
--- | Projections
-
+-- | Project the line number.
 lineno    :: Posn -> Int
+-- | Project the filename.
 filename  :: Posn -> String
+-- | Project the directory of the filename.
 directory :: Posn -> FilePath
 
 lineno    (Pn _ r _ _) = r
@@ -68,7 +75,7 @@ filename  (Pn f _ _ _) = f
 directory (Pn f _ _ _) = dirname f
 
 
--- | cpp-style printing
+-- | cpp-style printing of file position
 cppline :: Posn -> String
 cppline (Pn f r _ _) = "#line "++show r++" "++show f
 
