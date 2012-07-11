36680aa2664eac00561d8c1daf2037c37b7b5578
malcolm.wallace <malcolm.wallace@me.com>
2012-07-11 13:50:27 +0000

    Propagate the new API through some more modules.

--- Language/Preprocessor/Cpphs/RunCpphs.hs
+++ Language/Preprocessor/Cpphs/RunCpphs.hs
@@ -4,10 +4,12 @@
 -- Copyright (c) 2004 Malcolm Wallace
 -- This file is LGPL (relicensed from the GPL by Malcolm Wallace, October 2011).
 -}
-module Language.Preprocessor.Cpphs.RunCpphs ( runCpphs ) where
+module Language.Preprocessor.Cpphs.RunCpphs ( runCpphs
+                                            , runCpphsReturningSymTab
+                                            ) where
 
 import Language.Preprocessor.Cpphs.CppIfdef (cppIfdef)
-import Language.Preprocessor.Cpphs.MacroPass(macroPass)
+import Language.Preprocessor.Cpphs.MacroPass(macroPass,macroPassReturningSymTab)
 import Language.Preprocessor.Cpphs.Options  (CpphsOptions(..), BoolOptions(..))
 import Language.Preprocessor.Unlit as Unlit (unlit)
 
@@ -27,3 +29,21 @@ runCpphs options filename input = do
       pass3 = if literate bools then Unlit.unlit filename else id
 
   return (pass3 result)
+
+runCpphsReturningSymTab :: CpphsOptions -> FilePath -> String
+             -> IO (String,[(String,String)])
+runCpphsReturningSymTab options filename input = do
+  let bools  = boolopts options
+      preInc = case preInclude options of
+                 [] -> ""
+                 is -> concatMap (\f->"#include \""++f++"\"\n") is 
+                       ++ "#line 1 \""++filename++"\"\n"
+
+  pass1 <- cppIfdef filename (defines options) (includes options) bools
+                    (preInc++input)
+  (pass2,syms) <- macroPassReturningSymTab (defines options) bools pass1
+  let result= if not (macros bools) then unlines (map snd pass1) else pass2
+      pass3 = if literate bools then Unlit.unlit filename else id
+
+  return (pass3 result, syms)
+
