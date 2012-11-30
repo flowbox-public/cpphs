c051100519cedf5f7b7de422fc57682b8243b06b
Malcolm.Wallace <Malcolm.Wallace@me.com>
2012-11-30 10:52:48 +0000

    Bump version number to 1.15

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 LIBRARY = cpphs
-VERSION = 1.14
+VERSION = 1.15
 
 DIRS	= Language/Preprocessor/Cpphs \
 	  Text/ParserCombinators
--- cpphs.hs
+++ cpphs.hs
@@ -20,7 +20,7 @@ import Control.Monad  ( when )
 import Data.List   ( isPrefixOf )
 
 version :: String
-version = "1.14"
+version = "1.15"
 
 main :: IO ()
 main = do
